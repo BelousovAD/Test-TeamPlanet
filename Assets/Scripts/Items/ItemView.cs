f8a48cc912b58771de011f37a931c127f703ead7
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-08-11 13:49:07 +0800

    Исправлено отображение предмета при смене состояния

--- Assets/Scripts/Items/ItemView.cs
+++ Assets/Scripts/Items/ItemView.cs
@@ -10,24 +10,51 @@ namespace Items
         [SerializeField] private CanvasGroup _group;
 
         private Image _image;
+        private Item _item;
 
         private void Awake() =>
             _image = GetComponent<Image>();
 
         private void OnEnable()
         {
-            _provider.ItemChanged += UpdateView;
+            _provider.ItemChanged += UpdateSubscriptions;
+            UpdateSubscriptions();
+        }
+
+        private void OnDisable()
+        {
+            _provider.ItemChanged -= UpdateSubscriptions;
+            UpdateSubscriptions();
+        }
+
+        private void UpdateSubscriptions()
+        {
+            if (_item is not null)
+            {
+                Unsubscribe();
+            }
+
+            _item = _provider.Item;
+
+            if (_item is not null)
+            {
+                Subscribe();
+            }
+            
             UpdateView();
         }
 
-        private void OnDisable() =>
-            _provider.ItemChanged -= UpdateView;
+        private void Subscribe() =>
+            _item.StateChanged += UpdateView;
+
+        private void Unsubscribe() =>
+            _item.StateChanged -= UpdateView;
 
         private void UpdateView()
         {
-            _group.blocksRaycasts = _provider.Item is not null;
-            _group.alpha = _provider.Item is not null ? 1f : 0f;
-            _image.sprite = _provider.Item?.Sprite;
+            _group.blocksRaycasts = _item is not null;
+            _group.alpha = _item is not null ? 1f : 0f;
+            _image.sprite = _item?.Sprite;
         }
     }
 }
